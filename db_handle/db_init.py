5784b71e9bf690e79357046199c36e352d38db9e
Norvent <gordlab@mail.ru>
2026-03-17 20:02:43 +1000

    wraps into main

--- db_handle/db_init.py
+++ db_handle/db_init.py
@@ -13,18 +13,26 @@ DB_NAME = os.getenv("DBNAME")
 DB_PORT = os.getenv("PORT")
 
 
-con = psycopg2.connect(database='postgres', user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
-cur = con.cursor()
-con.autocommit = True
+def main():
+    try:
+        con = psycopg2.connect(database='postgres', user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+        cur = con.cursor()
+        con.autocommit = True
 
-try:
-    cur.execute(f"CREATE DATABASE {DB_NAME}")
+        cur.execute(f"CREATE DATABASE {DB_NAME}")
 
-except psycopg2.errors.DuplicateDatabase:
-    print("Database already exists")
+    except psycopg2.errors.DuplicateDatabase:
+        print("Database already exists")
 
-else:
-    print("Database successfully created")
+    except Exception as e:
+        print('Error: ', e)
 
-finally:
-    con.close()
+    else:
+        print("Database successfully created")
+
+    finally:
+        con.close()
+
+
+if __name__ == "__main__":
+    main()
--- db_handle/tables_init.py
+++ db_handle/tables_init.py
@@ -12,30 +12,35 @@ DB_HOST = os.getenv("HOST")
 DB_NAME = os.getenv("DBNAME")
 DB_PORT = os.getenv("PORT")
 
-try:
-    with psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT) as con:
-        cur = con.cursor()
+def main():
+    try:
+        with psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT) as con:
+            cur = con.cursor()
 
-        # Таблица users хранит информацию о пользователях. На данный момент только id и имя.
-        cur.execute("CREATE TABLE IF NOT EXISTS users "
-                    "(id SERIAL PRIMARY KEY, username VARCHAR(50), created_at TIMESTAMP)")
+            # Таблица users хранит информацию о пользователях. На данный момент только id и имя.
+            cur.execute("CREATE TABLE IF NOT EXISTS users "
+                        "(id SERIAL PRIMARY KEY, username VARCHAR(50), created_at TIMESTAMP)")
 
-        # Таблицу логов я решил не делать отдельной таблицей для каждого типа действия.
-        # log_id - идентификатор записи в таблице;
-        # user_id – пользователь, который совершает действие;
-        # action_type - тип совершенного действия (visit/register/login/logout/create_topic/view_topic/delete_topic/post);
-        # entity_id — сущность, над которой выполняется операция (например id созданного поста);
-        # is_success- успешное ли было завершение операции или нет;
-        # time - дата действия.
-        cur.execute("CREATE TABLE IF NOT EXISTS logs "
-                    "(log_id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id), "
-                    "action_type VARCHAR(30), entity_id INT NULL, is_success BOOLEAN, time TIMESTAMP)")
+            # Таблицу логов я решил не делать отдельной таблицей для каждого типа действия.
+            # log_id - идентификатор записи в таблице;
+            # user_id – пользователь, который совершает действие;
+            # action_type - тип совершенного действия (visit/register/login/logout/create_topic/view_topic/delete_topic/post);
+            # entity_id — сущность, над которой выполняется операция (например id созданного поста);
+            # is_success- успешное ли было завершение операции или нет;
+            # time - дата действия.
+            cur.execute("CREATE TABLE IF NOT EXISTS logs "
+                        "(log_id SERIAL PRIMARY KEY, user_id INT REFERENCES users(id), "
+                        "action_type VARCHAR(30), entity_id INT NULL, is_success BOOLEAN, time TIMESTAMP)")
 
-        # Я решил не создавать таблицу для entities, так как это будет избыточно и не требуется в тз.
-        # Также для каждой типа придется создавать свою таблицу и логику.
+            # Я решил не создавать таблицу для entities, так как это будет избыточно и не требуется в тз.
+            # Также для каждой типа придется создавать свою таблицу и логику.
 
-except Exception as e:
-    print('Error:', e)
+    except Exception as e:
+        print('Error:', e)
 
-else:
-    print(f'Tables created successfully')
+    else:
+        print(f'Tables created successfully')
+
+
+if __name__ == '__main__':
+    main()
