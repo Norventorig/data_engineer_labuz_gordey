ecaa4a01dbb997ebb66c59d84481c4a56258c157
Norvent <gordlab@mail.ru>
2026-03-18 16:44:04 +1000

    fixes imports

--- db_handle/core.py
+++ db_handle/core.py
@@ -1,9 +1,7 @@
 from db_init import main as db_init
-from tables_init import main as tables_init
 from db_gen import main as db_gen
 
 
 if __name__ == '__main__':
     db_init()
-    tables_init()
     db_gen()
